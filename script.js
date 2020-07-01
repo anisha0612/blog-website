b95f5adde47a36c6aa3c04b343059c03e86fcc8c
Anisha <anisha.0612@live.com>
2020-07-01 15:21:32 -0400

    increase in functionality of blog

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 import express from "express";
-import ejs from "ejs";
+import _ from "lodash";
 
 const app = express();
 const port = 3100;
+const posts = [];
 
 const homeStartingContent =
   "Now that the, uh, garbage ball is in space, Doctor, perhaps you can help me with my sexual inhibitions? With a warning label this big, you know they gotta be fun! Hey, what kinda party is this? There's no booze and only one hooker. Ummm…to eBay? We can't compete with Mom! Her company is big and evil! Ours is small and neutral!";
@@ -15,8 +16,56 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// home
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", { homeContent: homeStartingContent, posts: posts });
+});
+
+app.post("/", (req, res) => {
+  if (req.body.newPost === "home") {
+    res.render("compose");
+  } else {
+    res.render("home");
+  }
+});
+
+// about
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+// contact
+app.get("/contact", (req, res) => {
+  res.render("contact", { contact: contactContent });
+});
+
+// compose
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+  posts.push(post);
+  res.redirect("/");
+});
+
+// access posts
+
+app.get("/posts/:post", (req, res) => {
+  const requestedStr = _.lowerCase(req.params.post);
+  for (let post of posts) {
+    const postStr = _.lowerCase(post.title);
+    if (requestedStr === postStr) {
+      res.render("post", {
+        postTitle: post.title,
+        postContent: post.content,
+      });
+    }
+  }
 });
 
 app.listen(port, () => {
