9f64167a5ffabf4ec8a1779f77070fbe720f2230
Anisha <anisha.0612@live.com>
2020-07-03 17:24:06 -0400

    inclusion of mongoDb and complete functioning of website

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 import express from "express";
 import _ from "lodash";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ silent: process.env.NODE_ENV === "production" });
 
 const app = express();
-const port = 3100;
-// const posts = [];
 
-// const homeStartingContent =
-//   "Now that the, uh, garbage ball is in space, Doctor, perhaps you can help me with my sexual inhibitions? With a warning label this big, you know they gotta be fun! Hey, what kinda party is this? There's no booze and only one hooker. Ummm…to eBay? We can't compete with Mom! Her company is big and evil! Ours is small and neutral!";
-const aboutContent =
-  "The Knights Who Say Ni demand a sacrifice! We shall say 'Ni' again to you, if you do not appease us. Found them? In Mercia?! The coconut's tropical! How do you know she is a witch? Shh! Knights, I bid you welcome to your new home. Let us ride to Camelot! Ni! Ni! Ni! Ni! I'm not a witch. The nose? Look, my liege!";
+("The Knights Who Say Ni demand a sacrifice! We shall say 'Ni' again to you, if you do not appease us. Found them? In Mercia?! The coconut's tropical! How do you know she is a witch? Shh! Knights, I bid you welcome to your new home. Let us ride to Camelot! Ni! Ni! Ni! Ni! I'm not a witch. The nose? Look, my liege!");
 const contactContent =
   "I am the last of my species, and I know how that weighs on the heart so don't lie to me! Sorry, checking all the water in this area; there's an escaped fish. No… It's a thing; it's like a plan, but with more greatness.";
 
@@ -17,11 +14,16 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const uri = `mongodb+srv://admin-anisha:test123@cluster0.pnm2y.mongodb.net/blogDB?retryWrites=true&w=majority`;
+
 // Database connection
-mongoose.connect("mongodb://localhost:27017/blogsDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected…"))
+  .catch((err) => console.log(err));
 
 const Blog = mongoose.model("Blog", {
   title: String,
@@ -106,6 +108,6 @@ app.post("/delete", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at ${port}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running at 3000`);
 });
