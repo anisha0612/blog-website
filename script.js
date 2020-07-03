4806d63772a4c1a6e69b1c5377b0976b854985e5
Anisha <anisha.0612@live.com>
2020-07-02 20:01:01 -0400

    including mongoDb database

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 import express from "express";
 import _ from "lodash";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 3100;
-const posts = [];
+// const posts = [];
 
-const homeStartingContent =
-  "Now that the, uh, garbage ball is in space, Doctor, perhaps you can help me with my sexual inhibitions? With a warning label this big, you know they gotta be fun! Hey, what kinda party is this? There's no booze and only one hooker. Ummm…to eBay? We can't compete with Mom! Her company is big and evil! Ours is small and neutral!";
+// const homeStartingContent =
+//   "Now that the, uh, garbage ball is in space, Doctor, perhaps you can help me with my sexual inhibitions? With a warning label this big, you know they gotta be fun! Hey, what kinda party is this? There's no booze and only one hooker. Ummm…to eBay? We can't compete with Mom! Her company is big and evil! Ours is small and neutral!";
 const aboutContent =
   "The Knights Who Say Ni demand a sacrifice! We shall say 'Ni' again to you, if you do not appease us. Found them? In Mercia?! The coconut's tropical! How do you know she is a witch? Shh! Knights, I bid you welcome to your new home. Let us ride to Camelot! Ni! Ni! Ni! Ni! I'm not a witch. The nose? Look, my liege!";
 const contactContent =
@@ -16,9 +17,31 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Database connection
+mongoose.connect("mongodb://localhost:27017/blogsDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Blog = mongoose.model("Blog", {
+  title: String,
+  content: String,
+});
+
+const defaultTitle = "Aw, you're all Mr. Grumpy Face today.";
+const defaultContent =
+  "Now that the, uh, garbage ball is in space, Doctor, perhaps you can help me with my sexual inhibitions? With a warning label this big, you know they gotta be fun! Hey, what kinda party is this? There's no booze and only one hooker. Ummm…to eBay? We can't compete with Mom! Her company is big and evil! Ours is small and neutral!";
+
+const first = new Blog({ title: defaultTitle, content: defaultContent });
+// first.save();
+
 // home
 app.get("/", (req, res) => {
-  res.render("home", { homeContent: homeStartingContent, posts: posts });
+  Blog.find({}, (err, blogs) => {
+    if (!err) {
+      res.render("home", { posts: blogs });
+    }
+  });
 });
 
 app.post("/", (req, res) => {
@@ -45,11 +68,11 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  const post = {
+  const newBlog = new Blog({
     title: req.body.postTitle,
     content: req.body.postBody,
-  };
-  posts.push(post);
+  });
+  newBlog.save();
   res.redirect("/");
 });
 
@@ -57,15 +80,30 @@ app.post("/compose", (req, res) => {
 
 app.get("/posts/:post", (req, res) => {
   const requestedStr = _.lowerCase(req.params.post);
-  for (let post of posts) {
-    const postStr = _.lowerCase(post.title);
-    if (requestedStr === postStr) {
-      res.render("post", {
-        postTitle: post.title,
-        postContent: post.content,
+  Blog.find({}, (err, blogs) => {
+    if (!err) {
+      blogs.forEach((post) => {
+        const postStr = _.lowerCase(post.title);
+        if (requestedStr === postStr) {
+          res.render("post", {
+            postTitle: post.title,
+            postContent: post.content,
+          });
+        }
       });
     }
-  }
+  });
+});
+
+// delete posts
+
+app.post("/delete", (req, res) => {
+  Blog.findOneAndDelete({ title: req.body.post }, (err, post) => {
+    if (!err) {
+      console.log("successfully deleted");
+    }
+  });
+  res.redirect("/");
 });
 
 app.listen(port, () => {
